b0c886cf9c1bf09c791e43f26e0cebe4d01fe3b0
j- <jh@live.com.au>
2015-06-04 22:04:12 +1000

    Output CMYK values

--- scripts/views/output.js
+++ scripts/views/output.js
@@ -10,28 +10,36 @@ App.OutputView = App.BaseView.extend({
 	},
 	changeColor: function () {
 		var value = this.watch.get('color');
-		this.$('#output-preview').css('background-color', Color(value));
-		this.$('#output-decimal').val(value);
-		this.$('#output-red').val(Color.getRed(value));
-		this.$('#output-red-ratio').val(Color.getRed(value) / 0xff);
-		this.$('#output-green').val(Color.getGreen(value));
-		this.$('#output-green-ratio').val(Color.getGreen(value) / 0xff);
-		this.$('#output-blue').val(Color.getBlue(value));
-		this.$('#output-blue-ratio').val(Color.getBlue(value) / 0xff);
-		this.$('#output-hue').val(Color.getHue(value));
-		this.$('#output-hue-ratio').val(Color.getHue(value) / 360);
-		this.$('#output-saturation').val(Color.getSaturation(value) * 100);
-		this.$('#output-saturation-ratio').val(Color.getSaturation(value));
-		this.$('#output-lightness').val(Color.getLightness(value) * 100);
-		this.$('#output-lightness-ratio').val(Color.getLightness(value));
-		this.$('#output-string-hex').val(Color.formatHexString(value));
-		this.$('#output-string-hexshort').val(Color.formatShortHexString(value));
-		this.$('#output-string-rgb').val(Color.formatRGBString(value));
-		this.$('#output-string-rgb-percentage').val(Color.formatRGBPercentString(value));
-		this.$('#output-string-rgba').val(Color.formatRGBAString(value));
-		this.$('#output-string-rgba-percentage').val(Color.formatRGBAPercentString(value));
-		this.$('#output-string-hsl').val(Color.formatHSLString(value));
-		this.$('#output-string-hsla').val(Color.formatHSLAString(value));
+		var color = Color(value);
+		var rgb = Color.getRGBArray(color);
+		var hsl = Color.getHSLArray(color);
+		var cmyk = Color.getCMYKArray(color);
+		this.$('#output-preview').css('background-color', color);
+		this.$('#output-decimal').val(Number(color));
+		this.$('#output-red').val(rgb[Color.R]);
+		this.$('#output-red-ratio').val(rgb[Color.R] / 0xff);
+		this.$('#output-green').val(rgb[Color.G]);
+		this.$('#output-green-ratio').val(rgb[Color.G] / 0xff);
+		this.$('#output-blue').val(rgb[Color.B]);
+		this.$('#output-blue-ratio').val(rgb[Color.B] / 0xff);
+		this.$('#output-hue').val(hsl[Color.H]);
+		this.$('#output-hue-ratio').val(hsl[Color.H] / 360);
+		this.$('#output-saturation').val(hsl[Color.S] * 100);
+		this.$('#output-saturation-ratio').val(hsl[Color.S]);
+		this.$('#output-lightness').val(hsl[Color.L] * 100);
+		this.$('#output-lightness-ratio').val(hsl[Color.L]);
+		this.$('#output-cyan').val(cmyk[Color.C] / Color.CHANNEL_MAX * 100);
+		this.$('#output-magenta').val(cmyk[Color.M] / Color.CHANNEL_MAX * 100);
+		this.$('#output-yellow').val(cmyk[Color.Y] / Color.CHANNEL_MAX * 100);
+		this.$('#output-key').val(cmyk[Color.K] / Color.CHANNEL_MAX * 100);
+		this.$('#output-string-hex').val(Color.formatHexString(color));
+		this.$('#output-string-hexshort').val(Color.formatShortHexString(color));
+		this.$('#output-string-rgb').val(Color.formatRGBString(color));
+		this.$('#output-string-rgb-percentage').val(Color.formatRGBPercentString(color));
+		this.$('#output-string-rgba').val(Color.formatRGBAString(color));
+		this.$('#output-string-rgba-percentage').val(Color.formatRGBAPercentString(color));
+		this.$('#output-string-hsl').val(Color.formatHSLString(color));
+		this.$('#output-string-hsla').val(Color.formatHSLAString(color));
 	}
 });
 
